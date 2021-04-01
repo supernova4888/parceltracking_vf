df9985ae9a7bd284caa6745e1035dd01b7b0aaed
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-04-01 23:39:16 +0200

    files and structure set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./pages/Welcome";
+import "./pages/ListOfParcels";
+import "./pages/ParcelDetails";
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="#" alt="Welcome-img"/>
+        <h1>Parcel tracker</h1>
+        <p>Enter your parcel id to start</p>
+
+        <input placeholder="Search by parcel id" type="text"/>
       </header>
+
+
     </div>
   );
 }
-
-export default App;
