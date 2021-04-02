cacb60d016877e6a9b898b0fe5a7c8baa76468e7
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-04-02 15:19:15 +0200

    first mock up

--- src/pages/ParcelResult.jsx
+++ src/pages/ParcelResult.jsx
@@ -1,1 +1,10 @@
 // Search Results
+import React from 'react'
+
+export default function ParcelResult({parcel}) {
+    return (
+        <div>
+            {parcel.map(parcels => (<h1 key={parcel.id}>{parcel.status}</h1>))}
+        </div>
+    )
+}
